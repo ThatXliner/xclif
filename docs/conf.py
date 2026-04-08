cc41bdaafb57b56d15067c1ad144234db3b3f41b
ThatXliner <bryan.hu.2020@gmail.com>
2026-04-08 16:32:34 -0700

    forgot about the docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "Xclif"
 copyright = "2024, Bryan Hu"
 author = "Bryan Hu"
-release = "0.2.0"
+release = "0.3.0"
 
 extensions = [
     "sphinx.ext.autodoc",
