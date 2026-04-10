fd6089d84f6026b3109aa18e48b49a2279e10e70
ThatXliner <bryan.hu.2020@gmail.com>
2026-04-10 11:20:24 -0700

    bump to 0.4.3

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "Xclif"
 copyright = "2024, Bryan Hu"
 author = "Bryan Hu"
-release = "0.4.2"
+release = "0.4.3"
 
 extensions = [
     "sphinx.ext.autodoc",
